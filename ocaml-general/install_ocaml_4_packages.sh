3707a1b0b8f1c4f17dcca6b77f6b651221c952a1
kxc-wraikny <115773797+kxc-wraikny@users.noreply.github.com>
2023-02-21 18:05:03 +0900

    add OCaml 5.0.0 compatibility (#27)
    * add OCaml 5.0.0 compatibility
    
    * fix install scripts
    
    * fix install script filename specification
    
    * fix env var
    
    * fix Dockerfile
    
    * fix ocaml-general.yml
    
    * update ocaml-general.yml to use tee command
    
    * Update ocaml-general/Dockerfile
    
    Co-authored-by: Haochen M. Kotoi-Xie <hx@kxc.inc>
    
    ---------
    
    Co-authored-by: Haochen M. Kotoi-Xie <hx@kxc.inc>

---
+++ ocaml-general/install_ocaml_4_packages.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+opam install --yes \
+  dune.3.4.0 \
+  merlin \
+  odoc.2.2.0 \
+  ppxlib.0.26.0 \
+  ppx_deriving.5.2.1 \
+  js_of_ocaml.4.1.0 js_of_ocaml-ppx.4.1.0 js_of_ocaml-lwt.4.1.0 \
+  sexplib.v0.15.0 ppx_sexp_conv.v0.15.1 \
+  yojson.1.7.0 ppx_yojson_conv.v0.15.0 \
+  jsonm.1.0.1 \
+  ppx_optcomp.v0.15.0 \
+  brr.0.0.4 \
+  prr.0.1.1 \
+  zed.3.2.0 \
+  tezt.3.0.0 \
+  ocamlformat.0.24.1 \
+  ppx_inline_test.v0.15.0 \
+  alcotest.1.6.0 \
+  qcheck.0.19.1 \
+  qcheck-alcotest.0.19.1 \
+  && opam clean -y --logs --repo-cache --download-cache --switch-cleanup
