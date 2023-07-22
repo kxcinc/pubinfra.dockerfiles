5edf17ece403e6dc9e303bd69e023413b571a5ed
Haochen M. Kotoi-Xie <hx@kxc.inc>
2023-07-22 13:26:17 +0900

    upgrade to ppxlib.0.28.0, add melange.1.0.0, etc (#38)

--- ocaml-general/install_ocaml_4_packages.sh
+++ ocaml-general/install_ocaml_4_packages.sh
@@ -4,21 +4,22 @@ opam install --yes \
   dune.3.9.1 \
   merlin \
   odoc.2.2.0 \
-  ppxlib.0.26.0 \
-  bisect_ppx.2.8.2 \
+  ppxlib.0.28.0 \
+  bisect_ppx.2.8.3 \
   ppx_deriving.5.2.1 \
   js_of_ocaml.4.1.0 js_of_ocaml-ppx.4.1.0 js_of_ocaml-lwt.4.1.0 \
-  sexplib.v0.15.0 ppx_sexp_conv.v0.15.1 \
-  yojson.1.7.0 ppx_yojson_conv.v0.15.0 \
+  sexplib.v0.15.1 ppx_sexp_conv.v0.15.1 \
+  yojson.2.0.2 ppx_yojson_conv.v0.15.1 \
   jsonm.1.0.1 \
+  ezjsonm.1.3.0 \
   ppx_optcomp.v0.15.0 \
   brr.0.0.4 \
   prr.0.1.1 \
   zed.3.2.0 \
   tezt.3.0.0 \
-  ocamlformat.0.24.1 \
   ppx_inline_test.v0.15.0 \
   alcotest.1.6.0 \
-  qcheck.0.19.1 \
-  qcheck-alcotest.0.19.1 \
+  qcheck.0.20 \
+  qcheck-alcotest.0.20 \
+  melange.1.0.0 rescript-syntax.1.0.0 \
   && opam clean -y --logs --repo-cache --download-cache --switch-cleanup
